11cd31d08d67e28e0e909e119c490a02dcf0abdb
brokenSensor <jonimen95@yandex.ru>
2021-04-07 12:16:14 +0800

    Authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc Auth user and get token
-// @route POST /api/users/login
+// @route POST /api/user/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
@@ -25,4 +25,23 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser };
+// @desc Get user profile
+// @route POST /api/user/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
+});
+
+export { authUser, getUserProfile };
