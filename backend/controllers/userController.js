f7f447bccc3c15924f207153c319cff79bbc12a3
brokenSensor <jonimen95@yandex.ru>
2021-04-07 12:35:40 +0800

    Registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,39 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
+// @desc Register new user
+// @route POST /api/user
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExist = await User.findOne({ email });
+
+	if (userExist) {
+		res.status(400);
+		throw new Error('User alredy exist');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+});
+
 // @desc Get user profile
 // @route POST /api/user/profile
 // @access Private
@@ -44,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
