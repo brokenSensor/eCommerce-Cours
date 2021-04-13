6dce4add4d9cb04c3de89944bc70e7cc7418bd75
brokenSensor <jonimen95@yandex.ru>
2021-04-13 08:59:55 +0800

    Create and update products endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,9 +25,9 @@ const getProductById = asyncHandler(async (req, res) => {
 	}
 });
 
-// @desc Deleye a product
+// @desc DELETE a product
 // @route DELETE /api/products/:id
-// @access Public/Admin
+// @access Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id);
 
@@ -40,4 +40,63 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 });
 
-export { getProductById, getProducts, deleteProduct };
+// @desc Create a product
+// @route POST /api/products/
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample name',
+		price: 0,
+		user: req.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample brand',
+		category: 'Sample category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample description',
+	});
+
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		image,
+		brand,
+		category,
+		countInStock,
+		description,
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+		product.description = description;
+
+		const updatedProduct = await product.save();
+		res.json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
+
+export {
+	getProductById,
+	getProducts,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+};
