e4142345042357f3c1b0696f98dab8712f28204f
brokenSensor <jonimen95@yandex.ru>
2021-04-13 12:33:26 +0800

    Mark order as delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -97,10 +97,30 @@ const getOrders = asyncHandler(async (req, res) => {
 	res.json(orders);
 });
 
+// @desc Update order to delivered
+// @route PUT /api/orders/:id/deliver
+// @access Private/Admin
+const upadateOrderToDelivered = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id);
+
+	if (order) {
+		order.isDelivered = true;
+		order.deliveredAt = Date.now();
+
+		const updatedOrder = await order.save();
+
+		res.json(updatedOrder);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
+});
+
 export {
 	addOrderItems,
 	getOrderById,
 	upadateOrderToPaid,
 	getMyOrders,
 	getOrders,
+	upadateOrderToDelivered,
 };
